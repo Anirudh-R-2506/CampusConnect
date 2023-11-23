3c41865a15aad728d255afb51bb3c7fa06bf9a55
Anirudh R <anirudhnfs01@gmail.com>
2023-11-24 04:09:11 +0530

    Added: Databas Triggers

--- triggers.sql
+++ triggers.sql
@@ -24,13 +24,11 @@ CREATE TRIGGER before_insert_or_update_validate
 BEFORE INSERT OR UPDATE ON users
 FOR EACH ROW
 BEGIN
-    -- Check 'name' length
     IF CHAR_LENGTH(NEW.name) > 50 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Name must not exceed 50 characters';
     END IF;
 
-    -- Check 'username' length and uniqueness
     IF CHAR_LENGTH(NEW.username) > 60 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Username must not exceed 60 characters';
@@ -41,7 +39,6 @@ BEGIN
         SET MESSAGE_TEXT = 'Username must be unique';
     END IF;
 
-    -- Check 'email' length and uniqueness
     IF CHAR_LENGTH(NEW.email) > 225 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Email must not exceed 225 characters';
@@ -52,13 +49,11 @@ BEGIN
         SET MESSAGE_TEXT = 'Email must be unique';
     END IF;
 
-    -- Check 'role_id' existence in 'roles' table
     IF NOT EXISTS (SELECT 1 FROM roles WHERE id = NEW.role_id) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Invalid role_id. Role does not exist.';
     END IF;
 
-    -- Check 'password' length
     IF NEW.password IS NOT NULL AND CHAR_LENGTH(NEW.password) > 225 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Password must not exceed 225 characters';
@@ -69,19 +64,16 @@ CREATE TRIGGER before_insert_comment
 BEFORE INSERT ON comments
 FOR EACH ROW
 BEGIN
-    -- Check 'comment' length
     IF CHAR_LENGTH(NEW.comment) > 5000 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Comment must not exceed 5000 characters';
     END IF;
 
-    -- Check if 'post_id' references an existing post in 'posts' table
     IF NOT EXISTS (SELECT 1 FROM posts WHERE id = NEW.post_id) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Invalid post_id. Post does not exist.';
     END IF;
 
-    -- Check if 'user_id' references an existing user in 'users' table
     IF NOT EXISTS (SELECT 1 FROM users WHERE id = NEW.user_id) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Invalid user_id. User does not exist.';
@@ -92,19 +84,16 @@ CREATE TRIGGER before_insert_like
 BEFORE INSERT ON likes
 FOR EACH ROW
 BEGIN
-    -- Check if the combination of 'post_id' and 'user_id' is unique
     IF EXISTS (SELECT 1 FROM likes WHERE post_id = NEW.post_id AND user_id = NEW.user_id) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Duplicate like. User has already liked this post.';
     END IF;
 
-    -- Check if 'post_id' references an existing post in 'posts' table
     IF NOT EXISTS (SELECT 1 FROM posts WHERE id = NEW.post_id) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Invalid post_id. Post does not exist.';
     END IF;
 
-    -- Check if 'user_id' references an existing user in 'users' table
     IF NOT EXISTS (SELECT 1 FROM users WHERE id = NEW.user_id) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Invalid user_id. User does not exist.';
